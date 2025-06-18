458185de41f5eb0cd252935b8462fc873f7ed53a
Dilara <dilaraozturk028@gmail.com>
2025-06-18 15:27:28 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,83 @@
-import './Footer.css'
+import "./Footer.css"
 
-export default function Footer() {
+
+
+export default function Footer(){
+
+
+
     return(
-        <div className='Footer'>
-        </div>
+        <footer>
+            <section id="col-1">
+                <div>
+                    <ul>
+                    <h2>A propos de Pizza Hut</h2>
+                        <li>Magasins</li>
+                        <li>La marque Pizza Hut</li>
+                        <li>Emplois</li>
+                        <li>Franchise</li>
+                        <li>Contact</li>
+                        <li>Allergènes</li>
+                        <li>Opinion.pizzahut.be</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                    <h2>Commandez en ligne</h2>
+                        <li>Commandez Livraison</li>
+                        <li>Commandez pour Take out</li>
+                        <li>Mercredi En Ligne</li>
+                        <li>Mardi Malin</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                    <h2>Nos restaurants</h2>
+                        <li>Un Pizza Hut à proximité</li>
+                        <li>La carte de menu restaurant</li>
+                        <li>Bonus</li>
+                        <li>Kids</li>
+                        <li>Student Deals</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                    <h2>Actions</h2>
+                        <li>Mardi Malin</li>
+                        <li>Mercredi en ligne</li>
+                        <li>Jeudi Tout Compris</li>
+                        <li>Samedi Malin</li>
+                        <li>Pizza buffet</li>
+                        <li>Menu du Chef</li>
+                    </ul>
+                </div>
+            </section>
+            <section id="col-2">
+                <div id="div1">
+                   <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="100" height="40" fill="white" viewBox="0 0 24 24">
+                   <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+                   </svg>
+                   <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="100" height="40" fill="white" viewBox="0 0 24 24">
+                   <path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/>
+                   </svg>
+                   <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="100" height="40" fill="none" viewBox="0 0 24 24">
+                   <path fill="white" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+                   </svg>
+                </div>
+                <div>
+                    <div id="col-2-p">
+                        <p>© 2025 Pizza Hut</p>
+                        <p>Conditions de vente</p>
+                        <p>Conditions générales</p>
+                        <p>Conditions confidentialité</p>
+                        <p>Cookies</p>
+                    </div>
+                    <div id="col-2-p2">
+                        <p>Borsbeeksebrug 34, bus 4, 2600 Berchem-Pizza Belgium NV - 0428.600.141</p>
+                    </div>
+                </div>
+            </section>
+        </footer>
     )
 }
