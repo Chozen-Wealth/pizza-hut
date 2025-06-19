382acec7c3203a1e01194692b1dd6f9c2ab075ce
chozen <moustafa.eldehni@hotmail.com>
2025-06-19 12:07:58 +0200

    Panier css done et création de la page details vite fait

--- src/components/Panier/Panier.jsx
+++ src/components/Panier/Panier.jsx
@@ -1,4 +1,5 @@
 import './Panier.css'
+import moto from "../../assets/livraison.png"
 
 export default function Panier() {
     return(
@@ -14,12 +15,21 @@ export default function Panier() {
                     <div>€ 0,00</div>
                 </div>
             </div>
-                <button className='btnCommander'>
-                    <div style={{flexGrow: "0"}}>0</div>
-                    <div style={{flexGrow: "1"}}>Commander</div>
-                    <div style={{flexGrow: "0"}}>€0,00</div>
-                </button>
+            <button className='btnCommander'>
+                <div style={{flexGrow: "0"}}>0</div>
+                <div style={{flexGrow: "1"}}>Commander</div>
+                <div style={{flexGrow: "0"}}>€0,00</div>
+            </button>
+            <div className='infosCommander'>
+                <div>
+                    <img src={moto} alt="" />
+                    <h4>Commander</h4>
+                </div>
+                <p>
+                    Livraison à partir d'un montant minimum de commande de 15.00€.
+                </p>
 
+            </div>
         </div>
     )
 }
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom"
 import "./Details.css"
+import Panier from "../../components/Panier/Panier"
 
 
 
 export default function Details(){
 
-
+    const navigate = useNavigate()
 
     return(
-        <>
-        </>
+        <section className="Details">
+            <span onClick={()=> navigate("/")} className="retour">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+                Retour
+            </span>
+            <div className="DetailsGeneral">
+                <div className="DetailsPizza">
+
+                </div>
+                <Panier/>
+            </div>
+        </section>
     )
 }
