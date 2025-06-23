202865c7d2ce216835c2b6f5a08701858bf3f7a9
chozen <moustafa.eldehni@hotmail.com>
2025-06-23 12:34:51 +0200

    Scroll to top ajouté à chaque changement de page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 import { HashRouter } from 'react-router-dom'
+import ScrollTop from './components/ScrollTop/ScrollTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <HashRouter>
+        <ScrollTop />
         <App />
       </HashRouter>
     </Provider>
